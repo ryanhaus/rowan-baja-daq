04c6e25ee026467ab353d03f102f5f5e951d37e0
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-09-30 13:14:57 -0400

    Delete main_unit, start over

--- main_unit/software/src/custom_scale.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <lvgl.h>
-
-void setup_scale_custom(lv_obj_t*, lv_obj_t**, lv_obj_t**, lv_obj_t**);
-void update_scale_custom(lv_obj_t*, lv_obj_t*, lv_obj_t*, float);
--- main_unit/software/src/sensor.h
+++
@@ -1,8 +0,0 @@
-#include <zephyr/kernel.h>
-#include <math.h>
-
-float get_sensor_val(void)
-{
-    float uptime_s = (float)k_uptime_get() / 1000.0f;
-    return 25.0f * (0.5f + 0.5f * sinf(uptime_s));
-}
