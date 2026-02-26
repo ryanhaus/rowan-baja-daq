1f47c400439a8f3cbafa51b06b530158ca495b15
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-02-26 17:59:07 -0500

    can-module/software: Restructure software directory, add example library and targets dir

---
+++ can-module/software/lib/test_lib/test_lib.c
@@ -0,0 +1,11 @@
+#include "test_lib.h"
+
+int test_function(int a, int b)
+{
+    return a + b;
+}
+
+int test_function2(int a, int b)
+{
+    return a - b;
+}
---
+++ can-module/software/lib/test_lib/test_lib.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int test_function(int, int);
+int test_function2(int, int);
