3b52e101c94482ba6577ae79675fd9c0b89262d0
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-04-10 18:34:06 -0400

    can-module/software: Add vendor/mplab directory with empty gpio_base project

---
+++ can-module/software/vendor/mplab/gpio_base.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=gpio_base.X.a
+CND_ARTIFACT_PATH_default=dist/default/production/gpio_base.X.a
