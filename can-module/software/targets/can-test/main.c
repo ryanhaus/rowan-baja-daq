844b2f67f17ae4b43d0a324993e90cc0d95f8ea6
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-04-12 19:45:14 -0400

    can-module/software: Add can-test target

---
+++ can-module/software/targets/can-test/main.c
@@ -0,0 +1,55 @@
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <definitions.h>
+
+#define WRITE_ID(id) (id << 18)
+
+int main()
+{
+    // Initialize system & systick
+    SYS_Initialize(NULL);
+    SYSTICK_TimerStart();
+
+    // Initialize CAN
+    CAN0_Initialize();
+
+    static uint8_t can0_message_ram[CAN0_MESSAGE_RAM_CONFIG_SIZE] __attribute__((aligned (32)));
+    CAN0_MessageRAMConfigSet(can0_message_ram);
+
+    static uint8_t can0_tx_fifo[CAN0_TX_FIFO_BUFFER_SIZE];
+    CAN_TX_BUFFER* tx_buffer = (CAN_TX_BUFFER*)can0_tx_fifo;
+
+    // Control flags for CAN transceiver
+    GPIO_CAN_NSIL_Set();
+    GPIO_CAN_STANDBY_Clear();
+
+    // wait before starting
+    GPIO_LED_Set(); // LED off
+    SYSTICK_DelayMs(1000);
+
+    // build CAN packet
+    memset(can0_tx_fifo, 0x00, CAN0_TX_FIFO_BUFFER_ELEMENT_SIZE); // clear TX fifo
+    tx_buffer->id = WRITE_ID(0x469);
+    tx_buffer->dlc = 8; // 8 bytes
+    tx_buffer->fdf = 0;
+    tx_buffer->brs = 0;
+
+    uint32_t data[] = { 0xDEADBEEF, 0x12345678 };
+    memcpy(tx_buffer->data, (uint8_t*)data, sizeof(data));
+
+    // main loop: send CAN packets every 100 ms, blink LED while transmitting
+    // note: the LED's cathode is connected to PA12 (GPIO_LED), so setting the gpio to 0 turns on the LED, and setting to 1 turns it off
+    while (true)
+    {
+        GPIO_LED_Clear(); // LED on
+        bool can_success = CAN0_MessageTransmitFifo(1, tx_buffer);
+
+        SYSTICK_DelayMs(50);
+        GPIO_LED_Set(); // LED off
+                        
+        SYSTICK_DelayMs(can_success ? 100 : 500);
+    }
+
+    return -1;
+}
