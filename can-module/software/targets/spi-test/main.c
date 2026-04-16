24952e74387e4a652811d34e77adf1ebc62feb36
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-04-16 14:11:00 -0400

    can-module/software: Add spi-test target

---
+++ can-module/software/targets/spi-test/main.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <definitions.h>
+#include <board_led.h>
+
+int main()
+{
+    SYS_Initialize(NULL);
+    SYSTICK_TimerStart();
+    SERCOM0_SPI_Initialize();
+
+    char str[] = "Hello world!";
+
+    while (true)
+    {
+        LED_On();
+
+        SYSTICK_DelayMs(50);
+        SERCOM0_SPI_Write(str, sizeof(str));
+        LED_Off();
+
+        SYSTICK_DelayMs(200);
+    }
+
+    return -1;
+}
